ce94bdcedab4d61482984711ced32fd9d635ba6e
HongxiangLu <96686194+HongxiangLu@users.noreply.github.com>
2025-11-08 21:41:39 +0800

    Refactor Transformer model implementation

--- src/Transformer.py
+++ src/Transformer.py
@@ -1,3 +1,7 @@
+# coding=utf-8
+# Contact: bingquanxia@qq.com
+
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -14,15 +18,16 @@ def get_subsequent_mask(b: int, max_len: int, device: torch.device) -> torch.Ten
     """
     Args:
         b: batch-size.
-        max_len: the length of the whole sequence.
+        max_len: the length of the whole seqeunce.
         device: cuda or cpu.
     """
-    return torch.triu(torch.ones((b, max_len, max_len), device=device), diagonal=1).to(torch.bool)
+    return torch.triu(torch.ones((b, max_len, max_len), device=device), diagonal=1).to(torch.bool)     # or .to(torch.uint8)
 
 
 def get_enc_dec_mask(
-        b: int, max_feat_len: int, feat_lens: torch.Tensor, max_label_len: int, device: torch.device) -> torch.Tensor:
-    attn_mask = torch.zeros((b, max_label_len, max_feat_len), device=device)  # (b, seq_q, seq_k)
+    b: int, max_feat_len: int, feat_lens: torch.Tensor, max_label_len: int, device: torch.device
+) -> torch.Tensor:
+    attn_mask = torch.zeros((b, max_label_len, max_feat_len), device=device)       # (b, seq_q, seq_k)
     for i in range(b):
         attn_mask[i, :, feat_lens[i]:] = 1
     return attn_mask.to(torch.bool)
@@ -44,7 +49,7 @@ class MultiHeadAttention(nn.Module):
         self.d_v = d_v
         self.num_heads = num_heads
         self.dropout = nn.Dropout(p)
-
+        
         # linear projections
         self.W_Q = nn.Linear(d_model, d_k * num_heads)
         self.W_K = nn.Linear(d_model, d_k * num_heads)
@@ -52,6 +57,7 @@ class MultiHeadAttention(nn.Module):
         self.W_out = nn.Linear(d_v * num_heads, d_model)
 
         # Normalization
+        # References: <<Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification>>
         nn.init.normal_(self.W_Q.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
         nn.init.normal_(self.W_K.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
         nn.init.normal_(self.W_V.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_v)))
@@ -67,18 +73,18 @@ class MultiHeadAttention(nn.Module):
         Q = self.W_Q(Q).view(N, -1, num_heads, d_k).transpose(1, 2)
         K = self.W_K(K).view(N, -1, num_heads, d_k).transpose(1, 2)
         V = self.W_V(V).view(N, -1, num_heads, d_v).transpose(1, 2)
-
-        # pre-process mask
+        
+        # pre-process mask 
         if attn_mask is not None:
             assert attn_mask.size() == (N, q_len, k_len)
-            attn_mask = attn_mask.unsqueeze(1).repeat(1, num_heads, 1, 1)  # broadcast
+            attn_mask = attn_mask.unsqueeze(1).repeat(1, num_heads, 1, 1)    # broadcast
             attn_mask = attn_mask.bool()
 
         # calculate attention weight
         scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k)
         if attn_mask is not None:
             scores.masked_fill_(attn_mask, -1e4)
-        attns = torch.softmax(scores, dim=-1)  # attention weights
+        attns = torch.softmax(scores, dim=-1)        # attention weights
         attns = self.dropout(attns)
 
         # calculate output
@@ -89,6 +95,7 @@ class MultiHeadAttention(nn.Module):
         output = self.W_out(output)
 
         return output
+
 
 
 class PoswiseFFN(nn.Module):
@@ -102,9 +109,9 @@ class PoswiseFFN(nn.Module):
         self.dropout = nn.Dropout(p=p)
 
     def forward(self, X):
-        out = self.conv1(X.transpose(1, 2))  # (N, d_model, seq_len) -> (N, d_ff, seq_len)
+        out = self.conv1(X.transpose(1, 2))     # (N, d_model, seq_len) -> (N, d_ff, seq_len)
         out = self.relu(out)
-        out = self.conv2(out).transpose(1, 2)  # (N, d_ff, seq_len) -> (N, d_model, seq_len)
+        out = self.conv2(out).transpose(1, 2)   # (N, d_ff, seq_len) -> (N, d_model, seq_len)
         out = self.dropout(out)
         return out
 
@@ -120,7 +127,7 @@ class EncoderLayer(nn.Module):
             dropout_attn: dropout ratio of attention module
         """
         assert dim % n == 0
-        hdim = dim // n  # dimension of each attention head
+        hdim = dim // n     # dimension of each attention head
         super(EncoderLayer, self).__init__()
         # LayerNorm
         self.norm1 = nn.LayerNorm(dim)
@@ -170,8 +177,8 @@ class Encoder(nn.Module):
         self.layers = nn.ModuleList(
             [EncoderLayer(enc_dim, num_heads, dff, dropout_posffn, dropout_attn) for _ in range(num_layers)]
         )
-
-    def forward(self, X, mask=None):
+    
+    def forward(self, X, X_lens, mask=None):
         # add position embedding
         batch_size, seq_len, d_model = X.shape
         out = X + self.pos_emb(torch.arange(seq_len, device=X.device))  # (batch_size, seq_len, d_model)
@@ -181,7 +188,7 @@ class Encoder(nn.Module):
             out = layer(out, mask)
         return out
 
-
+    
 class DecoderLayer(nn.Module):
     def __init__(self, dim, n, dff, dropout_posffn, dropout_attn):
         """
@@ -242,7 +249,7 @@ class Decoder(nn.Module):
 
         # output embedding
         self.tgt_emb = nn.Embedding(tgt_vocab_size, dec_dim)
-        self.dropout_emb = nn.Dropout(p=dropout_emb)  # embedding dropout
+        self.dropout_emb = nn.Dropout(p=dropout_emb)                            # embedding dropout
         # position embedding
         self.pos_emb = nn.Embedding.from_pretrained(pos_sinusoid_embedding(tgt_len, dec_dim), freeze=True)
         # decoder layers
@@ -260,7 +267,7 @@ class Decoder(nn.Module):
         dec_out = self.dropout_emb(tgt_emb + pos_emb)
         # decoder layers
         for layer in self.layers:
-            dec_out = layer(dec_out, enc_out, dec_mask, dec_enc_mask)
+                dec_out = layer(dec_out, enc_out, dec_mask, dec_enc_mask)
         return dec_out
 
 
@@ -270,7 +277,7 @@ class Transformer(nn.Module):
             dec_out_dim: int, vocab: int,
     ) -> None:
         super().__init__()
-        self.frontend = frontend  # feature extractor
+        self.frontend = frontend     # feature extractor
         self.encoder = encoder
         self.decoder = decoder
         self.linear = nn.Linear(dec_out_dim, vocab)
@@ -281,7 +288,7 @@ class Transformer(nn.Module):
         device = X.device
         # frontend
         out = self.frontend(X)
-        max_feat_len = out.size(1)  # compute after frontend because of optional subsampling
+        max_feat_len = out.size(1)                            # compute after frontend because of optional subsampling
         max_label_len = labels.size(1)
         # encoder
         enc_mask = get_len_mask(b, max_feat_len, X_lens, device)
@@ -297,21 +304,21 @@ class Transformer(nn.Module):
 
 if __name__ == "__main__":
     # constants
-    batch_size = 16  # batch size
-    max_feat_len = 100  # the maximum length of input sequence
-    fbank_dim = 80  # the dimension of input feature
-    hidden_dim = 512  # the dimension of hidden layer
-    vocab_size = 26  # the size of vocabulary
-    max_lable_len = 100  # the maximum length of output sequence
+    batch_size = 16                 # batch size
+    max_feat_len = 100              # the maximum length of input sequence
+    fbank_dim = 80                  # the dimension of input feature
+    hidden_dim = 512                # the dimension of hidden layer
+    vocab_size = 26                 # the size of vocabulary
+    max_lable_len = 100             # the maximum length of output sequence
 
     # dummy data
-    fbank_feature = torch.randn(batch_size, max_feat_len, fbank_dim)  # input sequence
-    feat_lens = torch.randint(1, max_feat_len, (batch_size,))  # the length of each input sequence in the batch
-    labels = torch.randint(0, 26, (batch_size, max_lable_len))  # output sequence
-    label_lens = torch.randint(1, 10, (batch_size,))  # the length of each output sequence in the batch
+    fbank_feature = torch.randn(batch_size, max_feat_len, fbank_dim)        # input sequence
+    feat_lens = torch.randint(1, max_feat_len, (batch_size,))               # the length of each input sequence in the batch
+    labels = torch.randint(0, 26, (batch_size, max_lable_len))              # output sequence
+    label_lens = torch.randint(1, 10, (batch_size,))                        # the length of each output sequence in the batch
 
     # model
-    feature_extractor = nn.Linear(fbank_dim, hidden_dim)  # a single layer to simulate the audio feature extractor
+    feature_extractor = nn.Linear(fbank_dim, hidden_dim)                    # a single layer to simulate the audio feature extractor
     encoder = Encoder(
         dropout_emb=0.1, dropout_posffn=0.1, dropout_attn=0.,
         num_layers=6, enc_dim=hidden_dim, num_heads=8, dff=2048, tgt_len=2048
@@ -324,5 +331,4 @@ if __name__ == "__main__":
 
     # forward check
     logits = transformer(fbank_feature, feat_lens, labels)
-    print(logits.shape)  # (batch_size, max_label_len, vocab_size)
-
+    print(logits.shape)     # (batch_size, max_label_len, vocab_size)
